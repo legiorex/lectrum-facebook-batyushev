8602b0b14c68f5144a13409b96453afde1e32ab9
legiorex <batyushev@gmail.com>
2018-12-18 19:32:19 +1000

    Начал 12 урок

---
+++ source/components/Composer/Composer.test.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { mount, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { Composer } from './';
+
+configure({ adapter: new Adapter() });
+
+const props = {
+    _createPost: jest.fn(),
+};
+
+const comment = 'Merry christmas!';
+
+const initialState = {
+    comment: '',
+};
+
+const updatedState = {
+    comment,
+};
+
+const result = mount(<Composer { ...props } />);
+describe('composer component:', () => {
+    test('should have 1 "section" element', () => {
+        expect(result.find('section')).toHaveLength(1);
+    });
+
+    test('should have 1 "form" element', () => {
+        expect(result.find('form')).toHaveLength(1);
+    });
+
+    test('should have 1 "textarea" element', () => {
+        expect(result.find('textarea')).toHaveLength(1);
+    });
+
+    test('should have 1 "img" element', () => {
+        expect(result.find('img')).toHaveLength(1);
+    });
+
+    test('should have 1 "input" element', () => {
+        expect(result.find('input')).toHaveLength(1);
+    });
+
+    test('should have valid initial state', () => {
+        expect(result.state()).toEqual(initialState);
+    });
+
+    test('textarea value should be empty initially', () => {
+        expect(result.find('textarea').text()).toBe('');
+    });
+
+    test('should respond to state change properly', () => {
+        result.setState({
+            comment,
+        });
+        expect(result.state()).toEqual(updatedState);
+        expect(result.find('textarea').text()).toBe(comment);
+    });
+});
--- source/components/Composer/index.js
+++ source/components/Composer/index.js
@@ -5,8 +5,8 @@ import PropTypes from 'prop-types';
 // Instrumens
 import Styles from './styles.m.css';
 
-@withProfile
-export default class Composer extends Component {
+
+export class Composer extends Component {
     static propTypes = {
         _createPost: PropTypes.func.isRequired,
     };
@@ -50,18 +50,22 @@ export default class Composer extends Component {
         const { avatar, currentUserFirstName } = this.props;
 
         return (
-            <section className={Styles.composer}>
-                <img src={avatar} />
-                <form onSubmit={this._handleFormSubmit}>
+            <section className = { Styles.composer }>
+                <img src = { avatar } />
+                <form onSubmit = { this._handleFormSubmit }>
                     <textarea
-                        placeholder={` What\'s on your mind, ${currentUserFirstName}?`}
-                        value={comment}
-                        onChange={this._updateComment}
-                        onKeyPress={this._submitOnEnter}
+                        placeholder = { ` What\'s on your mind, ${currentUserFirstName}?` }
+                        value = { comment }
+                        onChange = { this._updateComment }
+                        onKeyPress = { this._submitOnEnter }
                     />
-                    <input type="submit" value="Post" />
+                    <input
+                        type = 'submit'
+                        value = 'Post'
+                    />
                 </form>
             </section>
         );
     }
 }
+export default withProfile(Composer);
